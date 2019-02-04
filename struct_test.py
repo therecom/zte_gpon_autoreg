f2483624f60b1fccfab3bc5fa6e100544169a94e
amindomao <amindomao@gmail.com>
2019-02-04 18:37:28 +0200

    Added abstract classes. Added one additional method.

--- struct_test.py
+++ struct_test.py
@@ -1,54 +1,84 @@
-"""Main module"""
-# import logging
+from abc import ABC, abstractmethod
 import paramiko
+"""Main module"""
 
 
-class Olt(paramiko.SSHClient):
+class Device:
+    pass
+
+
+class Olt(ABC, Device):
+
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def get_uncfg_onu(self):
+        pass
+
+    @abstractmethod
+    def get_free_slots(self):
+        pass
+
+    @abstractmethod
+    def register_onu(self):
+        pass
+
+    @abstractmethod
+    def get_onu_information(self, onu):
+        pass
+
+
+class OltBDCOM:
+    pass
+
+
+class OltZTE(paramiko.SSHClient, Olt):
 
     SLOTS = set(range(1, 129))
 
-    def __init__(self, host, **kwargs):
+    def __init__(self, host, username='', password=''):
         self.host = host
-        self.username = kwargs['username']
-        self.password = kwargs['password']
+        self.username = username
+        self.password = password
         super().__init__()
 
     def connect(self):
         """Tries to make ssh connection. Log if errors occurs """
+
         try:
-            super().connect(self.host, username=self.username,
+            super().connect(self.host,
+                            username=self.username,
                             password=self.password)
+
         except paramiko.AuthenticationException:
             print("Authentication error occured.")
+
         except paramiko.SSHException:
             print("Connection error occured.")
 
-        pass  # TODO
+        except paramiko.TimeouteError:
+            print("Timeout error occured.")
+
+        # FIXME
+        # logging
 
     def get_uncfg_onu(self):
         """Returns dict with PON ports as keys and uncfg ONUs' sn's lists as
         values: {pon_port1: [sn1, sn2, snN], ...}"""
-        pass  # TODO
+        pass  # FIXME
 
     def get_free_slots(self):
         """Returns dict with PON ports as keys and sorted lists of free
         slots as values"""
         # pon_ports = keys(self.uncfg_onu)
-        pass  # TODO
+        pass  # FIXME
 
     def register_onu(self):
         # onu_list = self.get_uncfg_onu()
         # free_slots = self.get_free_slots()
+        pass  # FIXME
 
-        # FIXME
-
-        self.close()
-        pass
-
-
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
+    def get_onu_information(self, onu):
+        pass  # FIXME
